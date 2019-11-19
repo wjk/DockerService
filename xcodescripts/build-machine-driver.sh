f33819ad46e717a4d883c9a9a3965728803a67ad
William Kent <wjk011@gmail.com>
2019-11-19 18:24:16 -0500

    Add custom script to build xhyve driver

---
+++ xcodescripts/build-machine-driver.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+set -e -x
+
+export GOPATH=${DERIVED_FILE_DIR}/go
+mkdir -p $GOPATH
+
+cd ${SRCROOT}/components/docker-machine-driver-xhyve
+go get github.com/zchee/docker-machine-driver-xhyve
+cd $GOPATH/src/github.com/zchee/docker-machine-driver-xhyve
+
+git checkout -- . # so the patches don't complain that they were already applied
+patch -p1 < ${SRCROOT}/files/xhyve-patches/fix-makefile.patch
+make build
+
+mkdir -p ${SRCROOT}/components/doker-machine-driver-xhyve
+cp bin/docker-machine-driver-xhyve ${SRCROOT}/components/docker-machine-driver-xhyve
